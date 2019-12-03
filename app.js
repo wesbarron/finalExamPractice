b54ca576276f5e9fa17e6ed7c98fd6e03899da7f
wesbarron <barron_wesley@columbusstate.edu>
2019-12-03 01:34:38 +0000

    changed url to https

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var http = require('http');
 var https = require('https');
 var nodeFetch = require('node-fetch');
+var fetch = require('isomorphic-fetch');
 
 var app = express();
 var port = process.env.PORT || 3000;
